189aee4a7f80e3bfe5e7954a11acb68091b879b2
Yashvanth S <yashv4475@gmail.com>
2026-03-22 14:12:18 +0530

    Add files via upload

---
+++ newmain.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include "clcd.h"
+#include "i2c.h"
+#include "adc.h"
+#include "rtc.h"
+#include "black.h"
+#include<string.h>
+#include "mkp.h"
+#include"uart.h"
+#include"eeprom.h"
+
+#endif
